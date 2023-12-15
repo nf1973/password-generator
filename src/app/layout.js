92c5b0f02d22ce3eaa41caded913918b1036e76a
nf1973 <56644716+nf1973@users.noreply.github.com>
2023-12-16 00:08:47 +0100

    layout improvements

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,11 +15,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <SSRProvider>
-          <main>
-            <Container className="py-4">{children}</Container>
-          </main>
-        </SSRProvider>
+        <main>
+          <div>{children}</div>
+        </main>
       </body>
     </html>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@
 
 import { useState, useCallback } from "react";
 import clipboardCopy from "clipboard-copy";
+import { Container } from "react-bootstrap";
 
 function getPassword(setPassword) {
   const apiUrl = "/api/getpassword";
@@ -26,6 +27,7 @@ function getPassword(setPassword) {
 
 export default function Home() {
   const [password, setPassword] = useState("");
+
   const getPasswordCallback = useCallback(
     () => getPassword(setPassword),
     [setPassword]
@@ -38,61 +40,84 @@ export default function Home() {
       );
     }
   };
-
   return (
-    <div className="container my-5">
-      <div className="row">
-        <div className="col-md-8 offset-md-2 text-center">
-          <h1 className="mb-4">Simple Password Generator</h1>
-          <p className="display-7 my-4">
-            {password ? (
-              <p className="display-9 my-4">{password}</p>
-            ) : (
-              <span>&nbsp;</span>
-              // or simply use {null} if you prefer no space
-            )}
-          </p>
-          <button
-            className={`btn mx-2 ${password ? "btn-secondary" : "btn-primary"}`}
-            onClick={getPasswordCallback}
-          >
-            Generate Password
-          </button>
-          <button
-            className={`btn mx-2 ${password ? "btn-primary" : "btn-secondary"}`}
-            onClick={password ? copyToClipboard : null}
-            disabled={!password}
-          >
-            Copy to Clipboard
-          </button>
-          <p className="mt-4">Created with ❤ in Next.js and Python</p>
+    <div>
+      <nav className="navbar navbar-expand navbar-dark bg-dark">
+        <div className="Container">
+          <a href="#" className="navbar-brand ps-3">
+            Simple Password Generator
+          </a>
+        </div>
+      </nav>
+      <div className="container my-5">
+        <div className="row">
+          <div className="col-md-8 offset-md-2 text-center">
+            <p className="lead">
+              This simple password generator will generate a strong password, 20
+              characters long.
+              <br /> Click the left button to create the password, and the right
+              button to copy to your clipboard
+            </p>
+            <div className="fs-5 my-4 border border-1 rounded">
+              {password ? (
+                <p className="py-4 mb-0 fw-bold">{password}</p>
+              ) : (
+                <p className="py-4 mb-0 fw-lighter fst-italic">
+                  Click the button below
+                </p>
+              )}
+            </div>
+            <button
+              className={`btn mx-2 ${
+                password ? "btn-secondary" : "btn-primary"
+              }`}
+              onClick={getPasswordCallback}
+            >
+              Generate Password
+            </button>
+            <button
+              className={`btn mx-2 ${
+                password ? "btn-primary" : "btn-secondary"
+              }`}
+              onClick={password ? copyToClipboard : null}
+              disabled={!password}
+            >
+              Copy to Clipboard
+            </button>
+            <p className="mt-4">Created with ❤ in Next.js and Python</p>
+          </div>
         </div>
       </div>
     </div>
 
-    // <div className="container">
-    //   <div className="row">
-    //     <div className="col-md-12 text-center">
-    //       <h1>Simple Password Generator</h1>
-    //       <h2>Generate a Strong Password</h2>
-    //       <p className="display-5">{password}</p>
-    //       <button
-    //         className="btn btn-primary mx-2"
-    //         onClick={getPasswordCallback}
-    //       >
-    //         Generate Password
-    //       </button>
-    //       {password && (
-    //         <button
-    //           className="btn btn-secondary mx-2"
-    //           onClick={copyToClipboard}
-    //         >
-    //           Copy to Clipboard
-    //         </button>
-    //       )}
-    //       <p>Created with ❤ in Next.js and Python</p>
-    //     </div>
-    //   </div>
-    // </div>
+    /* <div className="container my-5">
+<div className="row">
+  <div className="col-md-8 offset-md-2 text-center">
+    <h1 className="mb-4">Simple Password Generator</h1>
+    <p className="display-7 my-4">
+      {password ? (
+        <p className="display-9 my-4">{password}</p>
+      ) : (
+        <span>&nbsp;</span>
+        // or simply use {null} if you prefer no space
+      )}
+    </p>
+    <button
+      className={`btn mx-2 ${password ? "btn-secondary" : "btn-primary"}`}
+      onClick={getPasswordCallback}
+    >
+      Generate Password
+    </button>
+    <button
+      className={`btn mx-2 ${password ? "btn-primary" : "btn-secondary"}`}
+      onClick={password ? copyToClipboard : null}
+      disabled={!password}
+    >
+      Copy to Clipboard
+    </button>
+    <p className="mt-4">Created with ❤ in Next.js and Python</p>
+  </div>
+</div>
+</div> */
   );
 }
